f92a51d19bda865b87696588008c81f3157ceab2
Vadim Frolov <www.kivinet@mail.ru>
2021-03-10 15:23:17 +0300

    Домашнее задание больше деталей

--- js/main.js
+++ js/main.js
@@ -1,119 +1,114 @@
-const getRandomIntInclusive = function (min, max) {
+let DATA_ADS = {
+  AVATARS: 8,
+  TITLES: ['Одна комната', 'Одгокомнатная квартира', 'Двухкомнатная квартира', 'Трехкомнатная квартира', 'Пятикомнатная квартира', 'Одноэтажный дом', 'Двухэтажный дом', 'Трехэтажный дом'],
+  MIN_LOCATION_X: 35.65000,
+  MAX_LOCATION_X: 35.70000,
+  MIN_LOCATION_Y: 139.70000,
+  MAX_LOCATION_Y: 139.80000,
+  MIN_PRICE: 1000,
+  MAX_PRICE: 1000000,
+  TYPES: ['palace', 'flat', 'house', 'bungalo'],
+  MIN_ROOMS: 1,
+  MAX_ROOMS: 5,
+  MIN_GUEST: 1,
+  MAX_GUEST: 20,
+  CHECKING: ['12:00', '13:00', '14:00'],
+  CHECKOUT: ['12:00', '13:00', '14:00'],
+  FEATURES: ['wifi', 'dishwasher', 'parking', 'elevator', 'conditioner'],
+  DESCRIPTION: ['С видом на реку', 'С большой кухней', 'С балконом', 'С двумя санузлами', 'С одноместными кроватями', 'С двухместными краватями'],
+  PHOTOS: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+}
+
+// Функция возвращающая случайное число в диапазоне
+const getRandomNumber= function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-
   if (min >= max || min < 0 || max < 0) {
     throw new Error('Invalid Variable value');
   }
-
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
-const getRandomMapCoordination = function (min, max, numberOfDecimal = 5) {
+// Функция возвращающая случайное число в диапазоне с плавающей точкой
+const getRandomFloatingPointNumber = function (min, max, numberOfDecimal = 5) {
   if (max > min && min >= 0) {
     return +((Math.random() * (max - min) + min).toFixed(numberOfDecimal));
   }
-
   throw new Error('Invalid Variable value');
 }
 
-const TOTAL_USERS = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
-// Не понимаю как {{x}} привести в необходимое состояние. не перечисляя все значения массива.
-
-const ADRESS = ['location.x', 'location.y']; // Не понимаю через массив или через объект нужно сделать
-
-const PRICE = {
-  min: 500,
-  max: 100000,
-}; // Можно сдделать проще через getRandomIntInclusive(500, 100000), но тогда прийдется выносить в последнюю генерацию без getRandomIntInclusive, не знаю можно ли так сделать, т.к. не будет единообразия
-
-const assignTypes = () => {
-  const TYPES = ['palace', 'flat', 'house', 'bungalow'];
-  const types = [];
-  let typesLen = TYPES.length;
-
-  for (let i=getRandomIntInclusive(0, typesLen); i < typesLen; i+=getRandomIntInclusive(1, typesLen)) {
-    types.push(TYPES[i])
+// Функция, возвращающая случайный элемемент массива
+function getRandomElement(array) {
+  for (let i = 0; i < array.length; i++) {
+    let randomIndex = Math.floor(Math.random() * array.length);
   }
-
-  return types;
+  let randomElement = array[randomIndex];
+  return randomElement;
 }
 
-const ROOMS = {
-  min: 1,
-  max: 10,
-};
-
-const GUESTS = {
-  min: 1,
-  max: 20,
-};
-
-const assignCheckins = () => {
-  const CHECKING = ['12:00', '13:00', '14:00'];
-  const checkins = [];
-  let checkinsLen = CHECKING.length;
-
-  for (let i=getRandomIntInclusive(0, checkinsLen); i < checkinsLen; i+=getRandomIntInclusive(1, checkinsLen)) {
-    checkins.push(CHECKING[i])
-  }
-
-  return checkins;
+// Функция, создающая массив произвольной длины
+function getArrayLength(array) {
+  let clone = array.slice();
+  clone.length = getRandomNumber(1, array.length);
+  return clone;
 }
 
-const assignFeatures = () => {
-  const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  const features = [];
-  let featuresLen = FEATURES.length;
-
-  for (let i=getRandomIntInclusive(0, featuresLen); i < featuresLen; i+=getRandomIntInclusive(1, featuresLen)) {
-    features.push(FEATURES[i])
+// Функция, возвращающая новый массив из старого в случайном порядке
+function shuffleArray(array) {
+  let mixedArray = array.slice();
+  for (let i = mixedArray.length - 1; i > 0; i--) {
+    let randomIndex = Math.floor(Math.random() * (i + 1));
+    let tempValue = mixedArray[i];
+    mixedArray[i] = mixedArray[randomIndex];
+    mixedArray[randomIndex] = tempValue;
   }
-
-  return features;
+  return mixedArray;
 }
 
-const assignPhotos = () => {
-  const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-  const photos = [];
-  let photosLen = PHOTOS.length;
-
-  for (let i=getRandomIntInclusive(0, photosLen); i < photosLen; i+=getRandomIntInclusive(1, photosLen)) {
-    photos.push(PHOTOS[i])
+// Генерируем массив аватарок
+function generateAvatars() {
+  let listAvatars = [];
+  for (let i = 1; i < DATA_ADS.AVATARS + 1; i++) {
+    if (i < 10) {
+      i = '0' + i;
+    }
+    let avatars = 'img/avatars/user0' + i + '.png';
+    listAvatars.push(avatars);
   }
-
-  return photos;
+  return listAvatars;
 }
 
-const LOCATIONS = {
-  x: getRandomMapCoordination(35.65000, 35.70000),
-  y: getRandomMapCoordination(139.70000, 139.80000),
-}
+function generateAds() {
+  let ads = [];
+  let userAvatars = shuffleArray(generateAvatars());
+  let adHeadlines = shuffleArray(DATA_ADS.TITLES);
 
+  for (let i = 0; i < DATA_ADS.COUNT_USERS; i++) {
+    let locationX = getRandomFloatingPointNumber(DATA_ADS.MIN_LOCATION_X, DATA_ADS.MAX_LOCATION_X);
+    let locationY = getRandomFloatingPointNumber(DATA_ADS.MIN_LOCATION_Y, DATA_ADS.MAX_LOCATION_Y);
 
-// Думаю исходник должен выглядеть именно так по условиям дз. Пока как черновик.
-/*
-const generatingRandomArrayData = () => {
-  return {
-    author {
-      avatar: 'img/avatars/user0${getRandomIntInclusive(1, TOTAL_USERS)}.png',
-    },
-    offer {
-      title: 'Заголовок',
-      adress: '${location.x}, ${location.y}',
-      price: getRandomArrayElement(PRICE),
-      type: getRandomArrayElement(TYPE),
-      rooms: getRandomArrayElement(ROOMS),
-      guests: getRandomArrayElement(GUESTS),
-      checkin: getRandomArrayElement(CHECKING_TIME),
-      checkout: getRandomArrayElement(CHECKING_TIME),
-      features: getRandomArrayElement(FEATURES),
-      description: 'Эта жилплощадь подходит вамбольше всего',
-      photos: getRandomArrayElement(PHOTOS),
-    },
-    location {
-      coordinate: getRandomMapCoordination(LOCATIONS),
-    },
+    ads.push({
+      'author': {
+        'avatar': userAvatars[i],                           
+      },
+      'offer': {
+        'title': adHeadlines[i],
+        'adress': (locationX + ', ' + locationY),
+        'price': getRandomNumber(DATA_ADS.MAX_PRICE, DATA_ADS.MAX_PRICE),
+        'type': getRandomElement(DATA_ADS.TYPES),
+        'rooms': getRandomNumber(DATA_ADS.MIN_ROOMS, DATA_ADS.MAX_ROOMS),
+        'guests': getRandomNumber(DATA_ADS.MIN_GUEST, DATA_ADS.MAX_GUEST),
+        'checkin': getRandomElement(DATA_ADS.CHECKING),
+        'checkout': getRandomElement(DATA_ADS.CHECKOUT),
+        'features': getArrayLength(DATA_ADS.FEATURES),
+        'description': getRandomElement(DATA_ADS.DESCRIPTION),
+        'photos': getArrayLength(DATA_ADS.PHOTOS),
+      },
+      'location': {
+        'x': locationX,
+        'y': locationY,
+      },
+    });
   }
-};
-*/
+  return ads;
+}
